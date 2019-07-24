a1428f6cdcf05019321328814543b79684daa75c
Vladislav M <vladgeneralbox@gmail.com>
2019-07-24 15:47:28 +0300

    First task JavaOPs

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,65 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
+        System.out.println("All resume are removed");
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                System.out.println("Resume saved");
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume getValue = null;
+        for (Resume resume : storage) {
+            if (resume != null && resume.uuid.equals(uuid)) {
+                getValue = resume;
+                break;
+            }
+        }
+        return getValue;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                System.out.println("Resume deleted");
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        List<Resume> activeResume = new ArrayList<>();
+        for (Resume resume : storage) {
+            if (resume != null) {
+                activeResume.add(resume);
+            }
+        }
+        return activeResume.toArray(new Resume[0]);
     }
 
     int size() {
-        return 0;
+        int arraySize = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                arraySize++;
+            }
+        }
+        return arraySize;
     }
 }
