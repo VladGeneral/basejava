709358d89ef3b26c4543f541fb0d4a74bd8169cb
Vladislav M <vladgeneralbox@gmail.com>
2019-09-18 16:02:52 +0300

    HW7_3: JUL

--- src/com/urice/webapp/storage/AbstractStorage.java
+++ src/com/urice/webapp/storage/AbstractStorage.java
@@ -6,8 +6,11 @@ import com.urice.webapp.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SearchKey> implements Storage {
+
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     protected abstract SearchKey getSearchKey(String uuid);
 
@@ -25,24 +28,28 @@ public abstract class AbstractStorage<SearchKey> implements Storage {
 
     @Override
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         SearchKey searchKey = getNotExistedSearchKey(resume.getUuid());
         doSave(searchKey, resume);
     }
 
     @Override
     public void update(Resume resume) {
+        LOG.info("Update " + resume);
         SearchKey searchKey = getExistedSearchKey(resume.getUuid());
         doUpdate(searchKey, resume);
     }
 
     @Override
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SearchKey searchKey = getExistedSearchKey(uuid);
         return doGet(searchKey);
     }
 
     @Override
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SearchKey searchKey = getExistedSearchKey(uuid);
         doDelete(searchKey);
     }
@@ -50,6 +57,7 @@ public abstract class AbstractStorage<SearchKey> implements Storage {
     private SearchKey getExistedSearchKey(String uuid) {
         SearchKey searchKey = getSearchKey(uuid);
         if (!isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + "not exist");
             throw new NotExistStorageException(uuid);
         }
         return searchKey;
@@ -58,12 +66,14 @@ public abstract class AbstractStorage<SearchKey> implements Storage {
     private SearchKey getNotExistedSearchKey(String uuid) {
         SearchKey searchKey = getSearchKey(uuid);
         if (isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + "exist");
             throw new ExistStorageException(uuid);
         }
         return searchKey;
     }
 
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> list = doCopyAll();
         Collections.sort(list);
         return list;
