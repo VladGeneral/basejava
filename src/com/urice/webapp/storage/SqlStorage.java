a9fe98c148abf375f9fe3d47c9201f8481db3b67
Vladislav M <vladgeneralbox@gmail.com>
2020-05-19 12:09:43 +0300

    HW14: add contacts

--- src/com/urice/webapp/storage/SqlStorage.java
+++ src/com/urice/webapp/storage/SqlStorage.java
@@ -1,6 +1,7 @@
 package com.urice.webapp.storage;
 
 import com.urice.webapp.exception.NotExistStorageException;
+import com.urice.webapp.model.ContactType;
 import com.urice.webapp.model.Resume;
 import com.urice.webapp.sql.SqlHelper;
 
@@ -8,6 +9,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class SqlStorage implements Storage {
     private SqlHelper sqlHelper;
@@ -24,6 +26,14 @@ public class SqlStorage implements Storage {
             ps.execute();
             return null;
         });
+        for (Map.Entry<ContactType, String> e : resume.getContactMap().entrySet()) {
+            sqlHelper.<Void>execute("INSERT INTO contact (resume_uuid, type, value) VALUES (?,?,?)", ps -> {
+                ps.setString(1, resume.getUuid());
+                ps.setString(2, e.getKey().name());
+                ps.setString(3, e.getValue());
+                return null;
+            });
+        }
     }
 
     @Override
@@ -40,14 +50,25 @@ public class SqlStorage implements Storage {
 
     @Override
     public Resume get(String uuid) {
-        return sqlHelper.execute("SELECT * FROM resume r WHERE r.uuid =?", ps -> {
-            ps.setString(1, uuid);
-            ResultSet rs = ps.executeQuery();
-            if (!rs.next()) {
-                throw new NotExistStorageException(uuid);
-            }
-            return new Resume(uuid, rs.getString("full_name"));
-        });
+        return sqlHelper.execute("" +
+                        "SELECT * FROM resume r " +
+                        "LEFT JOIN contact c " +
+                        "ON r.uuid = c.resume_uuid " +
+                        "WHERE r.uuid =?",
+                ps -> {
+                    ps.setString(1, uuid);
+                    ResultSet rs = ps.executeQuery();
+                    if (!rs.next()) {
+                        throw new NotExistStorageException(uuid);
+                    }
+                    Resume resume = new Resume(uuid, rs.getString("full_name"));
+                    do {
+                        String value = rs.getString("value");
+                        ContactType contactType = ContactType.valueOf(rs.getString("type"));
+                        resume.setContact(contactType, value);
+                    } while (rs.next());
+                    return resume;
+                });
     }
 
     @Override
